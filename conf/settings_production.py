f0f6a2a162cc95058b7894d4ddc7370db3d6aabe
e <279967122@qq.com>
2018-08-25 16:51:59 +0800

    update

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -20,7 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'frameworkpro',                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
+        'USER': 'frameworkpro',                            # 你的数据库user
         'PASSWORD': 'frameworkpro@2018',                        # 你的数据库password
         'HOST': '172.50.19.22',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,7 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'frameworktest',                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
+        'USER': 'frameworktest',                            # 你的数据库user
         'PASSWORD': 'frameworktest@2018',                        # 你的数据库password
         'HOST': '172.50.19.22',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
